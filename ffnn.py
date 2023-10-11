07210e6e1ffdab0d7e58eeb45c56e89a132ac1a5
Rodney Finkel <111357994+RodneyFinkel@users.noreply.github.com>
2023-10-11 23:25:23 +0300

    model completed

--- ffnn.py
+++ ffnn.py
@@ -20,14 +20,17 @@ df = pd.read_csv('weatherAUS.csv', encoding='utf-8')
 # drop records where target RainTomorrow=NaN
 df = df.dropna(subset=['RainTomorrow'])
 
-# Fill missing values for numeric columns
-numeric_columns = df.select_dtypes(include=['number']).columns
+# Fill missing values for numeric columns only, using mean of column
+numeric_columns = df.select_dtypes(include=['number']).columns #.columns accesses the columns names attribute of the resulting dataframe
 df[numeric_columns] = df[numeric_columns].fillna(df[numeric_columns].mean())
+
 # create a flag for RainToday and RainTomorrow, note: RainTomorrowFlag will be our target variable
 df['RainTodayFlag'] = df['RainToday'].apply(lambda x: 1 if x == 'Yes' else 0)
 df['RainTomorrowFlag'] = df['RainTomorrow'].apply(lambda x: 1 if x == 'Yes' else 0)
 
-# using one input, [humidity3pm]
+
+
+# using one input, [humidity3pm], for the FFNN
 
 X = df[['Humidity3pm']] # double brackets are for obtaining a new dataframe and not just a series from the column Humidity3pm. To give the column more structure we want a dataframe
 y = df['RainTomorrowFlag'].values  # .values converts the series to a numpy array
@@ -42,7 +45,7 @@ model.add(Dense(2, activation='softplus', name="Hidden-Layer")) # softplus(x) = 
 model.add(Dense(1, activation='sigmoid', name='Output-Layer')) # sigmoid(x) = 1 / (1 + exp(-x)) (sigmoid function is for binary categorization)
 
 # Compile keras model
-model.compile(optimizer='adam', # default=RMSProp, algorithm to be used in backpropogation
+model.compile(optimizer='adam', # default=RMSProp, algorithm to be used in backpropogation. Adam is based on stochastic gradient descent (Adaptive movement estimation algorithm)
               loss='binary_crossentropy', # loss function to be optimized. A string(name of loss function) or a tf.keras.losses.Loss instance
               metrics=['Accuracy', 'Precision', 'Recall'], # List of metrics to be evaluated by the model during training and testing. Each of these can be a string(name of a built in function)
               loss_weights=None, # default=None, Optional list or dictionary specifying scalar coefficients(Python floats) to weigh the loss contributions of different model outputs
@@ -53,13 +56,78 @@ model.compile(optimizer='adam', # default=RMSProp, algorithm to be used in backp
 # fit keras model on the dataset
 model.fit(X_train, # input data
           y_train,  # target data
-          batch_size=10, 
-          
-          
-          )
+          batch_size=10, # Number of samples per gradient update. If unspecified, batch_size defaults to 32
+          epochs= 3, # default=1, an epoch is an iteration over the entire X and y data provided
+          verbose='auto', # default='auto' auto defaults to one(progress bar)
+          callbacks= None, # default none. see tf.keras.callbacks
+          validation_split=0.2, # fraction of the training data to be used as validation data. The model sets apart this fraction of the training data, will not train on it, and will evaluate the loss and any model metrics on this
+          shuffle=True, # default=True Boolean, whether to shuffle the training data before each epoch
+          class_weight={0 : 0.3, 1 : 0.7}, # default=None Optional dictionary mapping class indices to a weight. Used for weighting the loss function(during training only). This can be useful to tell the model to pay more attention to samples from an under-represented class
+          sample_weight=None, # default=None Optional numpy array of weight for the training samples, used for weighting the loss function(during training only)
+          initial_epoch=0, # Integer, default=0, Epoch at which to start training (useful for resuming a previous training run).
+          steps_per_epoch=None, # Integer or None, default=None, Total number of steps (batches of samples) before declaring one epoch finished and starting the next epoch. When training with input tensors such as TensorFlow data tensors, the default None is equal to the number of samples in your dataset divided by the batch size, or 1 if that cannot be determined. 
+          validation_steps=None, # Only relevant if validation_data is provided and is a tf.data dataset. Total number of steps (batches of samples) to draw before stopping when performing validation at the end of every epoch.
+          validation_batch_size=None, # Integer or None, default=None, Number of samples per validation batch. If unspecified, will default to batch_size.
+          validation_freq=3, # default=1, Only relevant if validation data is provided. If an integer, specifies how many training epochs to run before a new validation run is performed, e.g. validation_freq=2 runs validation every 2 epochs.
+          max_queue_size=10, # default=10, Used for generator or keras.utils.Sequence input only. Maximum size for the generator queue. If unspecified, max_queue_size will default to 10.
+          workers=1, # default=1, Used for generator or keras.utils.Sequence input only. Maximum number of processes to spin up when using process-based threading. If unspecified, workers will default to 1.
+          use_multiprocessing=False, # default=False, Used for generator or keras.utils.Sequence input only. If True, use process-based threading. If unspecified, use_multiprocessing will default to False. 
+         )
+
+
+# Use model to make predictions
+# Predict class labels on training data
+pred_labels_tr = (model.predict(X_train) > 0.5).astype(int)
+# Predict class labels on a test data
+pred_labels_te = (model.predict(X_test) > 0.5).astype(int)
+
+
+# Model Performance Summary
+print("")
+print('-------------------- Model Summary --------------------')
+model.summary() # print model summary
+print("")
+print('-------------------- Weights and Biases --------------------')
+for layer in model.layers:
+    print("Layer: ", layer.name) # print layer name
+    print("  --Kernels (Weights): ", layer.get_weights()[0]) # weights
+    print("  --Biases: ", layer.get_weights()[1]) # biases
+    
+print("")
+print('---------- Evaluation on Training Data ----------')
+print(classification_report(y_train, pred_labels_tr))
+print("")
 
 
 
+# Create 100 evenly spaced points from smallest X to largest X
+X_range = np.linspace(X.min(), X.max(), 100)
+# Predict probabilities for rain tomorrow
+y_predicted = model.predict(X_range.reshape(-1, 1))
 
+# Create a scatter plot
+fig = px.scatter(x=X_range.ravel(), y=y_predicted.ravel(), 
+                 opacity=0.8, color_discrete_sequence=['black'],
+                 labels=dict(x="Value of Humidity3pm", y="Predicted Probability of Rain Tomorrow",))
+
+# Change chart background color
+fig.update_layout(dict(plot_bgcolor = 'white'))
+
+# Update axes lines
+fig.update_xaxes(showgrid=True, gridwidth=1, gridcolor='lightgrey', 
+                 zeroline=True, zerolinewidth=1, zerolinecolor='lightgrey', 
+                 showline=True, linewidth=1, linecolor='black')
+
+fig.update_yaxes(showgrid=True, gridwidth=1, gridcolor='lightgrey', 
+                 zeroline=True, zerolinewidth=1, zerolinecolor='lightgrey', 
+                 showline=True, linewidth=1, linecolor='black')
+
+# Set figure title
+fig.update_layout(title=dict(text="Feed Forward Neural Network (1 Input) Model Results", 
+                             font=dict(color='black')))
+# Update marker size
+fig.update_traces(marker=dict(size=7))
+
+fig.show()
 
 
---
+++ ffnn_2inputs.py
@@ -0,0 +1,171 @@
+from tensorflow import keras # for buidling neural networks
+from keras.models import Sequential # for creating a linear stack of layers for the neural network
+from keras import Input # for instantiating a keras tensor
+from keras.layers import Dense # for creating regular densely-connected neural network
+
+import pandas as pd
+import numpy as np
+import sklearn # for model evaluation
+from sklearn.model_selection import train_test_split # for splitting data into training and test samples
+from sklearn.metrics import classification_report # for model evaluation metrics
+
+import plotly
+import plotly.express as px
+import plotly.graph_objects as go
+
+
+pd.options.display.max_columns=50
+df = pd.read_csv('weatherAUS.csv', encoding='utf-8')
+
+# drop records where target RainTomorrow=NaN
+df = df.dropna(subset=['RainTomorrow'])
+
+# Fill missing values for numeric columns only, using mean of column
+numeric_columns = df.select_dtypes(include=['number']).columns #.columns accesses the columns names attribute of the resulting dataframe
+df[numeric_columns] = df[numeric_columns].fillna(df[numeric_columns].mean())
+
+# create a flag for RainToday and RainTomorrow, note: RainTomorrowFlag will be our target variable
+df['RainTodayFlag'] = df['RainToday'].apply(lambda x: 1 if x == 'Yes' else 0)
+df['RainTomorrowFlag'] = df['RainTomorrow'].apply(lambda x: 1 if x == 'Yes' else 0)
+
+
+
+# Select data for multiviariate modeling, two inputs
+X=df[['WindGustSpeed', 'Humidity3pm']]
+y=df['RainTomorrowFlag'].values
+
+
+# Create training and testing samples
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
+
+
+# Specify the structure of a neural network
+model2 = Sequential(name="Model-with-Two-Inputs") # Model
+model2.add(Input(shape=(2,), name='Input-Layer')) # Input Layer - need to specify the shape of inputs
+model2.add(Dense(2, activation='softplus', name='Hidden-Layer')) # Hidden Layer, softplus(x) = log(exp(x) + 1)
+model2.add(Dense(1, activation='sigmoid', name='Output-Layer')) # Output Layer, sigmoid(x) = 1 / (1 + exp(-x))
+
+
+# Compile the keras model
+model2.compile(optimizer='adam', # default='rmsprop', an algorithm to be used in backpropagation
+              loss='binary_crossentropy', # Loss function to be optimized. A string (name of loss function), or a tf.keras.losses.Loss instance.
+              metrics=['Accuracy', 'Precision', 'Recall'], # List of metrics to be evaluated by the model during training and testing. Each of this can be a string (name of a built-in function), function or a tf.keras.metrics.Metric instance. 
+              loss_weights=None, # default=None, Optional list or dictionary specifying scalar coefficients (Python floats) to weight the loss contributions of different model outputs.
+              weighted_metrics=None, # default=None, List of metrics to be evaluated and weighted by sample_weight or class_weight during training and testing.
+              run_eagerly=None, # Defaults to False. If True, this Model's logic will not be wrapped in a tf.function. Recommended to leave this as None unless your Model cannot be run inside a tf.function.
+              steps_per_execution=None # Defaults to 1. The number of batches to run during each tf.function call. Running multiple batches inside a single tf.function call can greatly improve performance on TPUs or small models with a large Python overhead.
+             )
+
+
+# Fit keras model on the dataset
+model2.fit(X_train, # input data
+          y_train, # target data
+          batch_size=10, # Number of samples per gradient update. If unspecified, batch_size will default to 32.
+          epochs=3, # default=1, Number of epochs to train the model. An epoch is an iteration over the entire x and y data provided
+          verbose='auto', # default='auto', ('auto', 0, 1, or 2). Verbosity mode. 0 = silent, 1 = progress bar, 2 = one line per epoch. 'auto' defaults to 1 for most cases, but 2 when used with ParameterServerStrategy.
+          callbacks=None, # default=None, list of callbacks to apply during training. See tf.keras.callbacks
+          validation_split=0.2, # default=0.0, Fraction of the training data to be used as validation data. The model will set apart this fraction of the training data, will not train on it, and will evaluate the loss and any model metrics on this data at the end of each epoch. 
+          #validation_data=(X_test, y_test), # default=None, Data on which to evaluate the loss and any model metrics at the end of each epoch. 
+          shuffle=True, # default=True, Boolean (whether to shuffle the training data before each epoch) or str (for 'batch').
+          class_weight={0 : 0.3, 1 : 0.7}, # default=None, Optional dictionary mapping class indices (integers) to a weight (float) value, used for weighting the loss function (during training only). This can be useful to tell the model to "pay more attention" to samples from an under-represented class.
+          sample_weight=None, # default=None, Optional Numpy array of weights for the training samples, used for weighting the loss function (during training only).
+          initial_epoch=0, # Integer, default=0, Epoch at which to start training (useful for resuming a previous training run).
+          steps_per_epoch=None, # Integer or None, default=None, Total number of steps (batches of samples) before declaring one epoch finished and starting the next epoch. When training with input tensors such as TensorFlow data tensors, the default None is equal to the number of samples in your dataset divided by the batch size, or 1 if that cannot be determined. 
+          validation_steps=None, # Only relevant if validation_data is provided and is a tf.data dataset. Total number of steps (batches of samples) to draw before stopping when performing validation at the end of every epoch.
+          validation_batch_size=None, # Integer or None, default=None, Number of samples per validation batch. If unspecified, will default to batch_size.
+          validation_freq=3, # default=1, Only relevant if validation data is provided. If an integer, specifies how many training epochs to run before a new validation run is performed, e.g. validation_freq=2 runs validation every 2 epochs.
+          max_queue_size=10, # default=10, Used for generator or keras.utils.Sequence input only. Maximum size for the generator queue. If unspecified, max_queue_size will default to 10.
+          workers=1, # default=1, Used for generator or keras.utils.Sequence input only. Maximum number of processes to spin up when using process-based threading. If unspecified, workers will default to 1.
+          use_multiprocessing=False, # default=False, Used for generator or keras.utils.Sequence input only. If True, use process-based threading. If unspecified, use_multiprocessing will default to False. 
+         )
+
+
+# Use model to make predictions
+# Predict class labels on training data
+pred_labels_tr = (model2.predict(X_train) > 0.5).astype(int)
+# Predict class labels on a test data
+pred_labels_te = (model2.predict(X_test) > 0.5).astype(int)
+
+
+# Model Performance Summary
+print("")
+print('-------------------- Model Summary --------------------')
+model2.summary() # print model summary
+print("")
+print('-------------------- Weights and Biases --------------------')
+for layer in model2.layers:
+    print("Layer: ", layer.name) # print layer name
+    print("  --Kernels (Weights): ", layer.get_weights()[0]) # kernels (weights)
+    print("  --Biases: ", layer.get_weights()[1]) # biases
+    
+print("")
+print('---------- Evaluation on Training Data ----------')
+print(classification_report(y_train, pred_labels_tr))
+print("")
+
+print('---------- Evaluation on Test Data ----------')
+print(classification_report(y_test, pred_labels_te))
+print("")
+
+
+def Plot_3D(X, X_test, y_test, clf, x1, x2, mesh_size, margin):
+            
+    # Specify a size of the mesh to be used
+    mesh_size=mesh_size
+    margin=margin
+
+    # Create a mesh grid on which we will run our model
+    x_min, x_max = X.iloc[:, 0].min() - margin, X.iloc[:, 0].max() + margin
+    y_min, y_max = X.iloc[:, 1].min() - margin, X.iloc[:, 1].max() + margin
+    xrange = np.arange(x_min, x_max, mesh_size)
+    yrange = np.arange(y_min, y_max, mesh_size)
+    xx, yy = np.meshgrid(xrange, yrange)
+            
+    # Calculate Neural Network predictions on the grid
+    Z = model2.predict(np.c_[xx.ravel(), yy.ravel()])
+    Z = Z.reshape(xx.shape)
+
+    # Create a 3D scatter plot
+    fig = px.scatter_3d(x=X_test[x1], y=X_test[x2], z=y_test,
+                     opacity=0.8, color_discrete_sequence=['black'], height=900, width=1000)
+
+    # Set figure title and colors
+    fig.update_layout(#title_text="Scatter 3D Plot with FF Neural Network Prediction Surface",
+                      paper_bgcolor = 'white',
+                      scene_camera=dict(up=dict(x=0, y=0, z=1), 
+                                        center=dict(x=0, y=0, z=-0.1),
+                                        eye=dict(x=0.75, y=-1.75, z=1)),
+                                        margin=dict(l=0, r=0, b=0, t=0),
+                      scene = dict(xaxis=dict(title=x1,
+                                              backgroundcolor='white',
+                                              color='black',
+                                              gridcolor='#f0f0f0'),
+                                   yaxis=dict(title=x2,
+                                              backgroundcolor='white',
+                                              color='black',
+                                              gridcolor='#f0f0f0'
+                                              ),
+                                   zaxis=dict(title='Probability of Rain Tomorrow',
+                                              backgroundcolor='lightgrey',
+                                              color='black', 
+                                              gridcolor='#f0f0f0', 
+                                              )))
+    
+    # Update marker size
+    fig.update_traces(marker=dict(size=1))
+
+    # Add prediction plane
+    fig.add_traces(go.Surface(x=xrange, y=yrange, z=Z, name='FF NN Prediction Plane',
+                              colorscale='Bluered',
+                              reversescale=True,
+                              showscale=False, 
+                              contours = {"z": {"show": True, "start": 0.5, "end": 0.9, "size": 0.5}}))
+    fig.show()
+    return fig
+
+
+# Call the above function
+fig = Plot_3D(X, X_test, y_test, model2, x1='WindGustSpeed', x2='Humidity3pm', mesh_size=1, margin=0)
+
+
+
