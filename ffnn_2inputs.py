e2e41ed8c1ef3126dbd68fac2695844bb0a971ec
Rodney Finkel <111357994+RodneyFinkel@users.noreply.github.com>
2023-10-11 23:59:20 +0300

    model completed

--- ffnn_2inputs.py
+++ ffnn_2inputs.py
@@ -39,44 +39,43 @@ y=df['RainTomorrowFlag'].values
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
 
-# Specify the structure of a neural network
-model2 = Sequential(name="Model-with-Two-Inputs") # Model
-model2.add(Input(shape=(2,), name='Input-Layer')) # Input Layer - need to specify the shape of inputs
-model2.add(Dense(2, activation='softplus', name='Hidden-Layer')) # Hidden Layer, softplus(x) = log(exp(x) + 1)
-model2.add(Dense(1, activation='sigmoid', name='Output-Layer')) # Output Layer, sigmoid(x) = 1 / (1 + exp(-x))
+# 2-2-1 Neural Network
+model2 = Sequential(name="Model-with-Two-Inputs") 
+model2.add(Input(shape=(2,), name='Input-Layer')) 
+model2.add(Dense(2, activation='softplus', name='Hidden-Layer')) 
+model2.add(Dense(1, activation='sigmoid', name='Output-Layer')) 
 
 
 # Compile the keras model
-model2.compile(optimizer='adam', # default='rmsprop', an algorithm to be used in backpropagation
-              loss='binary_crossentropy', # Loss function to be optimized. A string (name of loss function), or a tf.keras.losses.Loss instance.
-              metrics=['Accuracy', 'Precision', 'Recall'], # List of metrics to be evaluated by the model during training and testing. Each of this can be a string (name of a built-in function), function or a tf.keras.metrics.Metric instance. 
-              loss_weights=None, # default=None, Optional list or dictionary specifying scalar coefficients (Python floats) to weight the loss contributions of different model outputs.
-              weighted_metrics=None, # default=None, List of metrics to be evaluated and weighted by sample_weight or class_weight during training and testing.
-              run_eagerly=None, # Defaults to False. If True, this Model's logic will not be wrapped in a tf.function. Recommended to leave this as None unless your Model cannot be run inside a tf.function.
-              steps_per_execution=None # Defaults to 1. The number of batches to run during each tf.function call. Running multiple batches inside a single tf.function call can greatly improve performance on TPUs or small models with a large Python overhead.
+model2.compile(optimizer='adam', 
+              loss='binary_crossentropy', 
+              metrics=['Accuracy', 'Precision', 'Recall'],  
+              loss_weights=None, 
+              weighted_metrics=None, 
+              run_eagerly=None, 
+              steps_per_execution=None 
              )
 
 
 # Fit keras model on the dataset
-model2.fit(X_train, # input data
-          y_train, # target data
-          batch_size=10, # Number of samples per gradient update. If unspecified, batch_size will default to 32.
-          epochs=3, # default=1, Number of epochs to train the model. An epoch is an iteration over the entire x and y data provided
-          verbose='auto', # default='auto', ('auto', 0, 1, or 2). Verbosity mode. 0 = silent, 1 = progress bar, 2 = one line per epoch. 'auto' defaults to 1 for most cases, but 2 when used with ParameterServerStrategy.
-          callbacks=None, # default=None, list of callbacks to apply during training. See tf.keras.callbacks
-          validation_split=0.2, # default=0.0, Fraction of the training data to be used as validation data. The model will set apart this fraction of the training data, will not train on it, and will evaluate the loss and any model metrics on this data at the end of each epoch. 
-          #validation_data=(X_test, y_test), # default=None, Data on which to evaluate the loss and any model metrics at the end of each epoch. 
-          shuffle=True, # default=True, Boolean (whether to shuffle the training data before each epoch) or str (for 'batch').
-          class_weight={0 : 0.3, 1 : 0.7}, # default=None, Optional dictionary mapping class indices (integers) to a weight (float) value, used for weighting the loss function (during training only). This can be useful to tell the model to "pay more attention" to samples from an under-represented class.
-          sample_weight=None, # default=None, Optional Numpy array of weights for the training samples, used for weighting the loss function (during training only).
-          initial_epoch=0, # Integer, default=0, Epoch at which to start training (useful for resuming a previous training run).
-          steps_per_epoch=None, # Integer or None, default=None, Total number of steps (batches of samples) before declaring one epoch finished and starting the next epoch. When training with input tensors such as TensorFlow data tensors, the default None is equal to the number of samples in your dataset divided by the batch size, or 1 if that cannot be determined. 
-          validation_steps=None, # Only relevant if validation_data is provided and is a tf.data dataset. Total number of steps (batches of samples) to draw before stopping when performing validation at the end of every epoch.
-          validation_batch_size=None, # Integer or None, default=None, Number of samples per validation batch. If unspecified, will default to batch_size.
-          validation_freq=3, # default=1, Only relevant if validation data is provided. If an integer, specifies how many training epochs to run before a new validation run is performed, e.g. validation_freq=2 runs validation every 2 epochs.
-          max_queue_size=10, # default=10, Used for generator or keras.utils.Sequence input only. Maximum size for the generator queue. If unspecified, max_queue_size will default to 10.
-          workers=1, # default=1, Used for generator or keras.utils.Sequence input only. Maximum number of processes to spin up when using process-based threading. If unspecified, workers will default to 1.
-          use_multiprocessing=False, # default=False, Used for generator or keras.utils.Sequence input only. If True, use process-based threading. If unspecified, use_multiprocessing will default to False. 
+model2.fit(X_train, 
+          y_train, 
+          batch_size=10, 
+          epochs=3, 
+          verbose='auto', 
+          callbacks=None, 
+          validation_split=0.2, 
+          shuffle=True, 
+          class_weight={0 : 0.3, 1 : 0.7}, 
+          sample_weight=None, 
+          initial_epoch=0, 
+          steps_per_epoch=None, 
+          validation_steps=None, 
+          validation_batch_size=None, 
+          validation_freq=3, 
+          max_queue_size=10, 
+          workers=1, 
+          use_multiprocessing=False, 
          )
 
 
